25f94255fb7de5876d84b2eb5d482d9f026baa47
Josh <josh@bemoty.dev>
2021-07-02 16:14:11 +0200

    404: made 404 work

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 require('dotenv').config()
 const express = require('express')
-const cookieParser = require('cookie-parser')
 const spotify = require('./spotify')
 
 const app = express()
 const spotifyApi = new spotify()
 const port = process.env.PORT
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+	res.sendFile('index.html')
+})
 
 app.get('/login', (req, res) => {
 	spotifyApi.handleLogin(req, res)
@@ -15,14 +20,8 @@ app.get('/callback', (req, res) => {
 	spotifyApi.handleCallback(req, res)
 })
 
-app.use(express.static('public')).use(cookieParser)
-
-app.get('/', (req, res) => {
-	res.sendFile('index.html')
-})
-
-app.use('*', (req, res) => {
-	res.status(404).sendFile('404.html')
+app.get('*', (req, res) => {
+	res.status(400).sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () => {
